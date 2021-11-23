6e9e9cfea8de838a46778def2fda756550adb9d7
Norman <chrisnormansbautista@gmail.com>
2021-11-23 20:29:25 +0800

    Add Login/Logoff functionality

---
+++ NewsPH/Controllers/AccountController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NewsPH.Data;
+using NewsPH.Models;
+using NewsPH.Models.ViewModels;
+using NewsPH.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPH.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signinManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> usermanager,
+            SignInManager<ApplicationUser> signinmanager, RoleManager<IdentityRole> rolemanager)
+        {
+            _db = db;
+            _userManager = usermanager;
+            _signinManager = signinmanager;
+            _roleManager = rolemanager;
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.User));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            //model.RoleName = Helper.Admin;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signinManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signinManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid login attempt");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Logoff()
+        {
+            await _signinManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ NewsPH/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPH.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 6)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirm password does not match")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [DisplayName("Role Name")]
+        public string RoleName { get; set; }
+    }
+}
